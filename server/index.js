5eedd8a537900c22ed0c6c1d4b1b96222447cf3c
이동건 <developer@idong-geon-ui-MacBookPro.local>
2022-09-07 13:54:36 +0900

    routes init & error handler

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/users.js";
+import commentRoutes from "./routes/comments.js";
+import videoRoutes from "./routes/videos.js";
+import authRoutes from "./routes/auth.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config();
@@ -15,6 +21,26 @@ const connect = () => {
       throw err;
     });
 };
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/videos", videoRoutes);
+app.use("/api/comments", commentRoutes);
+
+// error handler
+// app.use로 모두 연결되어 있기 때문에 next 함수를 사용하면
+// 호출되어 에러 헨들러가 작동됩니다.
+// ! app.use로 연결되는 next 함수 개념 공부하기
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Something went Wrong!";
+
+  return res.status(status).json({ success: false, status, message });
+});
 
 app.listen(8800, () => {
   connect();
